39a9a618aec24d3e925b01c84582152fc18efe24
Shoz1 <48013643+Shoz1@users.noreply.github.com>
2019-06-06 19:37:02 +0300

    Update Cpp66.cpp

--- Cpp66/Cpp66.cpp
+++ Cpp66/Cpp66.cpp
@@ -1,21 +1,60 @@
-﻿// Cpp66.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include "pch.h"
 #include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+struct shop {
+	string name;
+	int cost;
+	string unit;
+};
+
+void Init(shop *arr, const int n) {
+	for (int i = 0; i < n; i++) 
+	{
+		cout << "Input name: ";
+		cin >> arr[i].name;
+		cout << "Input cost: ";
+		cin >> arr[i].cost; cin.ignore();
+		cout << "Input unit: ";
+		cin >> arr[i].unit; cin.ignore();
+	}
+}
+
+void Print(shop *arr, const int n)
+{
+	cout << "\nList: " << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i].name << " | " << arr[i].cost << " | " << arr[i].unit << " | "<< endl;
+	}
+}
+
+void Write(shop *arr, const int n)
+{
+	ofstream file("file.txt", ios::binary | ios::in);
+	for (int i = 0; i < n; i++)
+	{
+		
+			file.write((char*)&arr[i].name, sizeof arr[i].name);
+			file.write((char*)&arr[i].cost, sizeof arr[i].cost);
+			file.write((char*)&arr[i].unit, sizeof arr[i].unit);
+		
+	}
+	file.close();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int n;
+	cout << "How many: ";
+	cin >> n;
+	shop *arr = new shop[n];
+	Init(arr, n);
+	Print(arr, n);
+	Write(arr, n);
+	delete[] arr;
+	return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
